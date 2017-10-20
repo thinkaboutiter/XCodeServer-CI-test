626744fd7cc0cf49b60ef64b3a03809aeb89ec0e
Boyan Yankov <boyan.yankov@igsoft.com>
2017-10-20 16:47:24 +0300

    updated `AppDelegate.swift` - added configuration for `SimpleLogger`

--- XCS-Demo/XCS-Demo/AppDelegate.swift
+++ XCS-Demo/XCS-Demo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SimpleLogger
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.configure_3rdParties()
+        
         return true
     }
 
@@ -40,7 +44,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+}
 
-
+fileprivate extension AppDelegate {
+    
+    func configure_3rdParties() {
+        self.configure_simpleLogger()
+    }
+    
+    func configure_simpleLogger() {
+        #if DEBUG
+            Logger.enableLogging(true)
+        #endif
+    }
 }
 
